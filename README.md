44b8cdb2f40ee66cb1a29af1add73c84238d453c
Donnie <donniepask@gmail.com>
2025-03-10 20:02:59 +0100

    changed package name, added readme and logo

---
+++ README.md
@@ -0,0 +1,26 @@
+# TEMPLAR
+
+<p align="center">
+  <img src="./images/logo.webp" alt="Templar Logo" width="150">
+</p>
+
+## Description
+This is the example repo for the tutorial [Create your custom npm template package](#) on dev.to.
+
+Templar is a powerful and lightweight Node.js package designed to bring order and structure to your projects, just like a noble knight of old. Whether you're handling templating, configurations, or structured data, Templar helps you maintain clean and organized code effortlessly.
+
+## Features
+- ⚔️ **Lightweight & Fast** – Minimal footprint, maximum efficiency.
+- 🏰 **Structured Approach** – Helps keep your scaffolding organized.
+- 🔥 **Easy Integration** – Just run the command and wait.
+
+
+## Usage
+```js
+npm create templar@latest
+```
+Then follow the instructions in the terminal. You will name your project and choose the scaffolding.
+At the moment there is only one scaffolding project made in VueJS. More will come soon.
+
+## License
+MIT License
--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 #!/usr/bin/env node
 
-import { init } from '@ricciodev/create-simple-test/utils/createProject.js';
-import { allowedPackages, starterKits, scope } from '@ricciodev/create-simple-test/utils/allowedPackages.js';
+import { init } from '@ricciodev/create-templar/utils/createProject.js';
+import { allowedPackages, starterKits, scope } from '@ricciodev/create-templar/utils/allowedPackages.js';
 import inquirer from 'inquirer';
+import chalk from 'chalk';
 
-(() => inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "projectName",
-            message: "Enter your project name",
-            default: "obi-wan-kenobi"
-        },
-        {
-            type: "list",
-            name: "prefix",
-            message: "Select a starter kit",
-            choices: [...starterKits]
-        }
-    ])
-    .then((answers) => {
-        const requestedPackage = `${scope}/${answers.prefix}-starter-kit`;
+(() => {
+    console.log(chalk.yellow(`Welcome to TEMPLAR!`))
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "projectName",
+                message: "Enter your project name",
+                default: "obi-wan-kenobi"
+            },
+            {
+                type: "list",
+                name: "prefix",
+                message: "Select a starter kit",
+                choices: [...starterKits]
+            }
+        ])
+        .then((answers) => {
+            const requestedPackage = `${scope}/${answers.prefix}-starter-kit`;
 
-        if (!allowedPackages().includes(requestedPackage)) {
-            throw new Error("Invalid package");
-        }
+            if (!allowedPackages().includes(requestedPackage)) {
+                throw new Error("Invalid package");
+            }
 
-        init(answers.projectName, requestedPackage);
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-            console.error("Cannot render the prompt...");
-        } else {
-            console.error(error.message);
-        }
-    }))();
+            init(answers.projectName, requestedPackage);
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                // Prompt couldn't be rendered in the current environment
+                console.error("Cannot render the prompt...");
+            } else {
+                console.error(error.message);
+            }
+        })
+})();
